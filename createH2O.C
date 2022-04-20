60b480f0ea13f925b2e7140c9bc4f2367c3683f8
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-04-20 18:43:27 +0200

    Update createH2O.C

--- createH2O.C
+++ createH2O.C
@@ -11,20 +11,31 @@ using namespace BALL;
 
 int main(int argc, char* argv[])
 {
+
+    // ## show help
+
     const char* RESET   = "\033[0m";
     const char* BBLUE    = "\033[1;34m";
     const char* BWHITE  = "\033[1;37m";
     const char* BRED    = "\033[1;31m";
-    cout  << BBLUE <<  "\n  Help:    " << BBLUE << " $ ./createH2O [angle] [filename] " ;
+    cout  <<  BBLUE <<  "\n  Help:    " << BBLUE << " $ ./createH2O [angle] [filename] " ;
     cout  <<  "\n  Example: " << BBLUE  << " $ ./createH2O 90.0 ../your_amazing_molecule.pdb" ;
     cout  <<  "\n  ";
     cout  <<  BWHITE << "\n  [angle]    | " << BWHITE  << " angle in degree " ;
     cout  <<  "\n  [file]     | " << BWHITE  << " output-file " << BRED << " !!! File path realitive !!!\n\n " << RESET;
 
-//B
+    // ## correct arg-count?
+
+    if (argc < 2) { cout  <<  BRED  <<  "\n   ==> Not enough arguments!   \n" << RESET; return 0; }
+    if (argc > 2) { cout  <<  BRED  <<  "\n   ==> Too many arguments!     \n" << RESET; return 0; }
+
+    // ## Create System and Molecule ...
+
+    // B)
     System s;
     BALL::Molecule m;
-//C
+
+    // C)
     BALL::Atom Ox;
     BALL::Atom H1;
     BALL::Atom H2;
@@ -46,6 +57,8 @@ int main(int argc, char* argv[])
     m.insert(H2);
 
 
+    // ## Set angle
+
     double angle_input  = atof(argv[1]);
     Angle angle(angle_input, false);
     Vector3 rotationaxis(1., 0., 0.);
@@ -53,14 +66,27 @@ int main(int argc, char* argv[])
     Matrix4x4 mat;
     mat.setRotation(angle, rotationaxis);
 
+    // ## Insert finished molecule in System
+
     s.insert(m);
 
 
-    // and write the file
+    // ## ... and write in the file
     std::string arg_out  = argv[2];
     PDBFile outfile(arg_out, ios::out);
     outfile << s;
     outfile.close();
+
+    /*
+                 (meow!)
+         /\_/\   )/
+   ((   (=^.^=)
+   ))    )   (
+    (( /      \    ' '
+     (   ) || ||     ' '
+     '----''-''-'  >+++°>
+
+    */
 
     return 0;
 }
