aeec2f026d6be2cb394b2c8b445f9d43ec46ecd2
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-04-23 18:56:45 +0200

    Update Space3D.cpp

--- Space3D.cpp
+++ Space3D.cpp
@@ -18,16 +18,21 @@ std::vector<NH_Group*> Space3D::search(double x, double y, double z) {
     std::set<int> neighbors_x = { abs(_x - 1) , _x , _x < Space[0][0].size() - 1 ? _x + 1 : _x};
     std::set<int> neighbors_y = { abs(_y - 1) , _y , _y < Space[0].size() - 1 ? _y + 1 : _y};
     std::set<int> neighbors_z = { abs(_z - 1) , _z , _z < Space.size() - 1 ? _z + 1 : _z};
-    std::cout << "\n search at " << x << " " << y << " " << z;
+    
+    //std::cout << "\n search at " << x << " " << y << " " << z;
+    
     std::vector<NH_Group*> res;
 
     for(int _x_ : neighbors_x) {
         for(int _y_ : neighbors_y) {
             for(int _z_ : neighbors_z) {
-                std::cout << "\n (x,y,z) " << _x_ << " " << _y_ << " " << _z_;
+                
+                //std::cout << "\n (x,y,z) " << _x_ << " " << _y_ << " " << _z_;
 
-                for(NH_Group NH : this->Space[_x_][_y_][_z_].NH_Groups) {
+                for(NH_Group NH : this->Space[_z_][_y_][_x_].NH_Groups) {
+                    
                     res.push_back(NH);                    
+                    
                 }
             }
         }
@@ -51,7 +56,7 @@ void Space3D::pushToSpace(double x, double y, double z,NH_Group* NH) {
     int _x = (int)(x / boxsize);
     int _y = (int)(y / boxsize);
     int _z = (int)(z / boxsize);
-    this->Space[_x][_y][_z].NH_Groups.push_back(value);
+    this->Space[_z][_y][_x].NH_Groups.push_back(value);
 }
 
 /*
@@ -65,5 +70,5 @@ void Space3D::pushToSpace(double x, double y, double z,CO_Group* CO) {
     int _x = (int)(x / boxsize);
     int _y = (int)(y / boxsize);
     int _z = (int)(z / boxsize);
-    this->Space[_x][_y][_z].CO_Groups.push_back(value);
+    this->Space[_z][_y][_x].CO_Groups.push_back(value);
 }
