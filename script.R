e45d396b781133a35899724c1ff59caf4be912e7
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-04-30 01:53:52 +0200

    Update script.R

--- script.R
+++ script.R
@@ -1,16 +1,34 @@
 # 
 #
+# Command line:
+#
+# $ Rscript test.R [plot_name] [inputfile]
+#
+# $ Rscript test.R coffee plotList.csv
+#
 #
 
-inputfile = "X:\\BioInfo_2\\BI_Programmierung\\03_Ramachandran\\plotList.csv"
+# -- read arguments --
 
-outputfile = "X:\\BioInfo_2\\BI_Programmierung\\03_Ramachandran\\yay.png"
+args = commandArgs(trailingOnly = TRUE)
 
-# -- program start and read in --
+# arg-check
+if (length(args)!=2) {
+  stop("\n | You need exactly two arguments: \n | $ Rscript test.R [plot_name] [inputfile]\n | example: \n | $ Rscript test.R coffee plotList.csv\n\n", call.=FALSE)
+}
+
+name <- args[1]
+filename <- args[2]
+output <- paste(name,".png", sep = "", collapse = NULL)
+
+# read file
+coffee <- read.csv(filename, header = TRUE, sep=";")
+tuplecount <- length(count.fields(filename)) - 1
+
+# -- Create Plot --
+
+#install.packages("ggplot2")
 library(ggplot2)
-# install.packages("ggplot2")
-coffee <- read.csv(inputfile, header = TRUE, sep=";")
-str(coffee)
 
 # plot the data
 
@@ -22,5 +40,12 @@ plot + geom_density_2d_filled(aes(y = phi, x = psi))
 # , aes(x=phi, y=psi),color="steelblue")
 # plot + geom_point(aes(x = phi, y = psi, size = v1), fill="green", shape=21)
 
-ggsave(outputfile) 
+ggsave(output) 
 
+# -- Output inputs --
+
+getinputs <- function(){
+str <- cat("\n R A M A C H A N D R A N - P L O T \n Your inputs   : ", length(args),"args\n plot name     : ",name,"\n input file    : ",filename,"\n count tuples  : ",tuplecount,"\n\n -> output saved : '",output,"'\n")
+}
+
+getinputs()
