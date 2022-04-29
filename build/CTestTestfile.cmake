6090b6515e6d6792701731b63f1f324b95250a09
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-04-29 05:05:16 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/masy00001/Documents/bioinfo2/Uebung01
-# Build directory: /home/masy00001/Documents/bioinfo2/Uebung01/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
