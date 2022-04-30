345f61d65b9ea16fcfca6cd8b4055c072e0edafa
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-04-30 09:02:24 +0200

    Update console.cpp

--- console/console.cpp
+++ console/console.cpp
@@ -76,8 +76,8 @@ void console::ShowAngles(std::string outputfile, std::vector<AnglePair> AP) {
     std::cout << C::BWHITE  <<    "\n" << " | ";
     std::cout << C::BWHITE  <<    "\n" << " | " << C::BGREEN  << "You can create a Plot with the R-File 'script.R'";
     std::cout << C::BWHITE  <<    "\n" << " | " << C::BGREEN  << "please call:";
-    std::cout << C::BWHITE  <<    "\n" << " | " << C::GREEN   << "$ Rscript test.R [plot_name] [inputfile]";
-    std::cout << C::BWHITE  <<    "\n" << " | " << C::RED     << "example: $ Rscript test.R xyZ72 plotList.csv\n\n" << C::RESET;
+    std::cout << C::BWHITE  <<    "\n" << " | " << C::GREEN   << "$ Rscript script.R [plot_name] [inputfile]";
+    std::cout << C::BWHITE  <<    "\n" << " | " << C::RED     << "example: $ Rscript script.R xyZ72 plotList.csv\n\n" << C::RESET;
 
 }
 
