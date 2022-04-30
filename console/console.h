16b36a75a56e7ea598e9368e262f8127cecb89db
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-04-30 13:36:11 +0200

    Update console.h

--- console/console.h
+++ console/console.h
@@ -11,6 +11,7 @@ class console {
 public:
 
  static void ShowHeader(std::string filename, std::string protein);
+ static char ShowChoices(std::string filename,std::string proteinname);
  static void Help(std::string name,std::string message);
  static void ShowAngles(std::string outputfile, std::vector<AnglePair> AP);
 
