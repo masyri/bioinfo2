0a0238cd85facb72423c1b423766c086b04d633f
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-05-01 11:56:20 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,16 @@
 // Created by manjaro on 29.04.22.
 //
 
+/**
+ *
+ *  ##   R A M A C H A N D R A N - P L O T   ##
+ *
+ *  Start this program with ./ramachandran without arguments
+ *  all data (File ...) statistics and file outputs can you
+ *  control with the main menues
+ *
+ */
+
 #include <BALL/KERNEL/system.h>
 #include <BALL/KERNEL/molecule.h>
 #include <BALL/KERNEL/atom.h>
@@ -41,17 +51,29 @@ int main(int argc, char* argv[])
         if (chosen == '1') {
             file = false;
             while (!file) {
+                // wait for filename typing in ...
                 cout << C::BWHITE << "Type PDB-File in and press Enter: " << C::BRED << "(type ESC and Enter to abort)\n" << C::RESET;
                 cin >> filename;
+                // check first char ESC ... -> ABORT
                 if (filename.begin().operator*() == '\033') {file = false;break;}
                 std::ifstream infile(filename);
+                // check file is valid ...
                 if (infile) {
+                    // file .pdb ?
+                    if (filename.find(".pdb") == string::npos) {
+                        cout << C::BRED << "\n > your file is not a  *.pdb - File  ’" << filename << "’ \n" << C::RESET;
+                        continue;}
+                    // file okay -> set file true and continue to program
                     file = true;
-                    continue;} else {
+                    continue;
+                    // file not exist -> return to loop
+                    } else {
                     cout << C::BRED << "\n > invalid filename ’" << filename << "’ \n" << C::RESET;}
             }
+            // if Abort, set file false
             if (!file) {proteinname  = "---";filename     = "---";continue;}
-                        
+
+            // open file ...
             PDBFile f(filename, ios::in);
             System S;
             f >> S;
@@ -63,7 +85,7 @@ int main(int argc, char* argv[])
 
         // Print sequences
         if (chosen == '2') {
-                if (!file) {cout << C::BRED << " > No valid PDB File loaded.\n > press Enter to continue ...";getchar();continue;}
+                if (!file) {cout << C::BRED << " > No valid PDB File loaded.\n";console::pressY2continue();continue;}
                 cout << C::BBLUE   << " > Print sequences ...\n\n";
             
                 PDBFile f(filename, ios::in);
@@ -72,61 +94,62 @@ int main(int argc, char* argv[])
                 Ramachandran R(S);
             
                 R.printSequences();
-                std::cout << C::BLUE   <<    "\n\n" << " > press [ENTER] to continue ... \n";
-                getchar();
+                cout << "\n";
+                console::pressY2continue();
                 continue;
             }
 
         // Print stats in percent
         if (chosen == '3') {
-            if (!file) {cout << C::BRED << " > No valid PDB File loaded.\n > press Enter to continue ...";getchar();continue;}
-            cout << C::BBLUE   << " > Print stats ...\n";
-            
-            PDBFile f(filename, ios::in);
-            System S;
-            f >> S;
-            Ramachandran R(S);
-            
-            R.printStats();
-            std::cout << C::BLUE   <<    "\n" << " > press [ENTER] to continue ... \n";
-            getchar();
-            continue;
+                if (!file) {cout << C::BRED << " > No valid PDB File loaded.\n";console::pressY2continue();continue;}
+                cout << C::BBLUE   << " > Print stats ...";
+
+                PDBFile f(filename, ios::in);
+                System S;
+                f >> S;
+                Ramachandran R(S);
+
+                R.printStats();
+                console::pressY2continue();
+                continue;
             }
 
         // Print Angles
         if (chosen == '4') {
-            if (!file) {cout << C::BRED << " > No valid PDB File loaded.\n > press Enter to continue ...";getchar();continue;}
-            cout << C::BBLUE   << " > Print all Psi/Phi angles ...\n";
-            
-            PDBFile f(filename, ios::in);
-            System S;
-            f >> S;
-            Ramachandran R(S);
-            
-            auto angles = R.getTorsionAngels();
-            console::ShowAngles(output,angles);
-            continue;}
+                if (!file) {cout << C::BRED << " > No valid PDB File loaded.\n";console::pressY2continue();continue;}
+                cout << C::BBLUE   << " > Print all Psi/Phi angles ...\n";
+
+                PDBFile f(filename, ios::in);
+                System S;
+                f >> S;
+                Ramachandran R(S);
+
+                auto angles = R.getTorsionAngels();
+                console::ShowAngles(output,angles);
+                console::pressY2continue();
+                continue;
+            }
 
         // Create CSV File
         if (chosen == '5') {
-            if (!file) {cout << C::BRED << " > No valid PDB File loaded.\n > press Enter to continue ...";getchar();continue;}
-            cout << C::BBLUE << " > Create CSV file ...\n";
-            
-            PDBFile f(filename, ios::in);
-            System S;
-            f >> S;
-            Ramachandran R(S);
-            
-            auto angles = R.getTorsionAngels();
-            Ramachandran::anglesToFile(output,angles);
-            cout << C::BBLUE   << " > File created '" << output << "'\n";
-            cout << C::BWHITE  <<   "\n" << " > " << C::BMAGENTA  << " ---> You can create a Plot with the R-File 'script.R'";
-            cout << C::BWHITE  <<   "\n" << " > " << C::BMAGENTA  << "please call:";
-            cout << C::BWHITE  <<   "\n" << " > " << C::BMAGENTA  << "$ Rscript test.R [plot_name] [inputfile]";
-            cout << C::BWHITE  <<   "\n" << " > " << C::BMAGENTA  << "example: $ Rscript script.R xyZ72 plotList.csv\n\n" << C::RESET;
-            cout << C::BLUE   <<    "\n" << " > press [ENTER] to continue ... \n";
-            getchar();
-            continue;
+                if (!file) {cout << C::BRED << " > No valid PDB File loaded.\n";console::pressY2continue();continue;}
+                cout << C::BBLUE << "\n > Create CSV file ...\n";
+
+                PDBFile f(filename, ios::in);
+                System S;
+                f >> S;
+                Ramachandran R(S);
+
+                auto angles = R.getTorsionAngels();
+                Ramachandran::anglesToFile(output,angles);
+                cout << C::BBLUE   << " > File created '" << C::BWHITE << output << "'\n" << C::RESET;
+                cout << C::BWHITE  <<   "\n" << "   " << C::BMAGENTA  << "You can create a Plot with the R-File 'script.R'";
+                cout << C::BWHITE  <<   "\n" << "   " << C::BMAGENTA  << "please call:\n";
+                cout << C::BWHITE  <<   "\n" << "   " << C::BMAGENTA  << "$ Rscript script.R [plot_name] [inputfile]\n";
+                cout << C::BWHITE  <<   "\n" << "   " << C::BMAGENTA  << "example: $ Rscript script.R xyZ72 plotList.csv\n\n" << C::RESET;
+
+                console::pressY2continue();
+                continue;
             }
 
         // break the loop and close the program
