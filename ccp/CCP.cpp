399d0a9d3851d50a51945055cfb9f3f596d161a2
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-05-13 11:32:46 +0200

    Add files via upload

--- ccp/CCP.cpp
+++ ccp/CCP.cpp
@@ -7,6 +7,62 @@
 Occurrence CCP::createOccurrenceMatrixFromFiles(vector<string> files) {
     Occurrence O;
 
+        for (string file : files) {
+
+        // == open file
+
+        PDBFile meop(file, ios::in);
+        System S;
+        meop >> S;
+
+        // == iterate over all proteins of the system
+        for(ProteinIterator iter = S.beginProtein(); +iter; ++iter) {
+
+            // ## get protein
+            Protein *protein = (S.getProtein(index));
+            index++;
+
+            // ## iterate over proteins
+
+            for (ResidueIterator res_iter_A = protein->beginResidue(); +res_iter_A; ++res_iter_A) {
+
+                if(res_iter->isAminoAcid()){
+
+                    // -- get Name
+                    char code = 'Kaffee';
+
+                    // -- counter for contacts
+                    int contacts = 0;
+
+                    // -- get reference Atom
+                    Atom A = res_iter_A->getAtom("CA");
+
+                    // -- look for neighbors 
+                    for (ResidueIterator res_iter_B = protein->beginResidue(); +res_iter_B; ++res_iter_B){
+
+                        if(res_iter_B->isAminoAcid()){
+
+                            Atom B = res_iter_B->getAtom("CA");
+
+                            bool not_same = res_iter_B != res_iter;
+
+                            bool in_distance = distance( A , B , 7 ); 
+
+                            if (not_same && in_distance) {contact++;}
+
+                        }
+                    }
+
+                    // -- insert occurrence
+                    O.insert(code,contacts);
+
+                }
+
+            }
+        }
+
+    }
+
 
 
 
@@ -15,3 +71,12 @@ Occurrence CCP::createOccurrenceMatrixFromFiles(vector<string> files) {
 
 
 
+
+
+bool distance(Atom A, Atom B, double max) {
+
+double dist = A.getDistance(B);
+if (dist <= max) return true;
+return false;
+
+}
--- ccp/CCP.h
+++ ccp/CCP.h
@@ -7,11 +7,27 @@
 
 
 #include "Occurrence.h"
+#include <BALL/KERNEL/system.h>
+#include <BALL/KERNEL/molecule.h>
+#include <BALL/KERNEL/atom.h>
+#include <BALL/KERNEL/bond.h>
+#include <BALL/KERNEL/PTE.h>
+#include <BALL/STRUCTURE/secondaryStructureProcessor.h>
+#include <BALL/STRUCTURE/fragmentDB.h>
+#include <BALL/STRUCTURE/peptides.h>
+#include <BALL/FORMAT/PDBFile.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+using namespace BALL;
 
 class CCP {
 public:
 
     static Occurrence createOccurrenceMatrixFromFiles(vector<string> files);
+
+    static bool distance(Atom A, Atom B, double max);
 
 };
 
