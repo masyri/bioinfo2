30c6fe24a9a25024c0742bdf754967a57bb48aeb
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-06-03 00:39:46 +0200

    Update RMSD_Test.cpp

--- tests/RMSD_Test.cpp
+++ tests/RMSD_Test.cpp
@@ -15,7 +15,9 @@
 
 
 TEST(SPACE,getCenter) {
+    
     Space d;
+    
     d.addPosition("a",4,0,0);
     d.addPosition("a",8,0,0);
     d.addPosition("a",-4,0,0);
@@ -31,26 +33,35 @@ TEST(SPACE,getCenter) {
     d.addPosition("a",0,0,-4);
     d.addPosition("a",0,0,-8);
 
-Vector3 s = d.calcCenterCoordinate(); // -> (0,0,0)
+    Vector3 center = d.calcCenterCoordinate(); // -> (0,0,0)
+    
+EXPECT_TRUE(false);
 
 }
 
 
+
+
+
 TEST(SPACE,MoveCenter) {
 
-Space S;
+    Space S;
 
-S.addPosition("O",3,3,2);   // -> (0,0,0)
-S.addPosition("a",3,5,-3);  // -> (0,2,-5)
-S.addPosition("b",3,2,4);   // -> (0,-1,2)
-S.addPosition("c",-2,-2,2); // -> (-5,-5,0)
-S.addPosition("z",0,0,0);   // -> (-3,-3,-2)
+    S.addPosition("O",3,3,2);   // -> (0,0,0)
+    S.addPosition("a",3,5,-3);  // -> (0,2,-5)
+    S.addPosition("b",3,2,4);   // -> (0,-1,2)
+    S.addPosition("c",-2,-2,2); // -> (-5,-5,0)
+    S.addPosition("z",0,0,0);   // -> (-3,-3,-2)
+    
+    Vector3 newCenter(3,3,2);
+       
+    S.moveCenterCoordinate(newCenter);
 
-Position _1 = S.positions[0];
-Position _2 = S.positions[1];
-Position _3 = S.positions[2];
-Position _4 = S.positions[3];
-Position _5 = S.positions[4];
+    Position _1 = S.positions[0];
+    Position _2 = S.positions[1];
+    Position _3 = S.positions[2];
+    Position _4 = S.positions[3];
+    Position _5 = S.positions[4];
 
 EXPECT_TRUE(_1.index == "0" && _1.x == 0 && _1.y == 0 && _1.z == 0 );
 EXPECT_TRUE(false);
