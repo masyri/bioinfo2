1818bb59f5f5c1012c159641bddcf148737f6d84
Mareike Syring <masy00001@stud.uni-saarland.de>
2022-06-15 15:33:01 +0200

    update

--- Main.R
+++ Main.R
@@ -20,6 +20,40 @@ training_set <- getTrain(genes,split)
 
 test_set <- getTest(genes,split)
 
+drugs <- openTable('data/drug_response.txt')
+
+split_drug <- Splitter(drugs,0.8)
+
+training_set_drug <- getTrain(drugs,split_drug)
+
+test_set_drug <- getTest(drugs,split_drug)
+
+# ii
+## 5-Fold Crossvalidation ##
+trC <- trainControl(method = 'cv', number = 5)
+
+## mtry ###
+tunegrid <- expand.grid(.mtry=c(1:70))
+
+## Train ##
+fit <- train(training_set_drug~., data = training_set, method = 'rf', metric = "ROC", tuneGrid=tunegrid, trControl = trC)
+
+
+# iv
+## predict ##
+predict(fit, newdata = test_set, type = 'prob')
+
+## confusionmatrix ##
+confusionMatrix(data= test_set$pred, reference = test_set$obs)
+
+
+#v
+## Feature Importance-Plot ##
+plot(varImp(fit))
+
+
+
+
 ## Train
 
 #rdaClasses <- predict(rdaFit, newdata = test_set)
