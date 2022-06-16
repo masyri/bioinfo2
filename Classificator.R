0fe6148216f08f7370e39b43aa487127c32be0c9
andybioinfo <93399473+andybioinfo@users.noreply.github.com>
2022-06-17 00:11:17 +0200

    Add files via upload

---
+++ Classificator.R
@@ -0,0 +1,21 @@
+#
+#  ``` $ ./Rscript Classificator.R <gene expression> <drug> <training set 80%> <test set 20%>``` 
+#
+
+
+source("data.R")
+
+drugs <- openTable('data/IC50_data.txt')
+
+drugs <- openTable('data/expression_data.txt')
+
+
+
+
+
+
+
+
+
+
+
