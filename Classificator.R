3ef0312c847b85a9691c191544acf310a80d3219
Mareike Syring <masy00001@stud.uni-saarland.de>
2022-06-19 16:58:18 +0200

    update

--- Classificator.R
+++ Classificator.R
@@ -3,6 +3,7 @@
 #
 library(randomForest)
 library(caret)
+library(tidyselect)
 library(mltools)
 source("data.R")
 
@@ -21,36 +22,44 @@ training_set <- openTable(input@training_set)
 test_set <- openTable(input@test_set)         
 genes <- openTable(input@gene_expressions)
 
+## get current Drug Name
+drug_name = names(drugs)[1]
 
+## feature selection
+## selektieren von bestimmten Krebsgenen 
+cancer.genes <- openTable('data/cancer_gene_list.txt')
 
+str(cancer.genes[1])
+
+cancer.genes = as.vector(cancer.genes)
+genes <- subset(genes, select = c(TP53, PIK3CA, KRAS))
+
+## reduse cell-line 
+# bind same cell-lines
+gesamt <- merge(x= genes,y = drugs, by = "row.names")
+#erase NA values
+gesamt = gesamt[complete.cases(gesamt), ]
 
 ## Bind and Split
 
-genes2 <- cbind(genes, drugs)
+split <- Splitter(gesamt,0.8)
 
-split <- Splitter(genes,0.8)
+training_set <- getTrain(gesamt,split)
 
-training_set <- getTrain(genes,split)
+test_set <- getTest(gesamt,split)
 
-test_set <- getTest(genes,split)
-
-## feature selection
-cancer.genes <- openTable('data/cancer_gene_list.txt')
-
-as.vector(cancer.genes)
-genes <- subset(genes, select = c(cancer.genes))
 
 ## 5-Fold Crossvalidation ##
 trC <- trainControl(method = 'cv', number = 5, classProbs = TRUE, summaryFunction = twoClassSummary)
 
 ## mtry ###
-tunegrid <- expand.grid(.mtry=c(1:70))
+tunegrid <- expand.grid(.mtry=c(1:3))
 
-training_set$CASP10 = factor(training_set$CASP10)
-levels(training_set$CASP10) <- make.names(levels(factor(training_set$CASP10)))
+training_set$Camptothecin = factor(training_set$Camptothecin)
+levels(training_set$Camptothecin) <- make.names(levels(factor(training_set$Camptothecin)))
 
 ## Train ##
-fit <- train(CASP10~., data = training_set, method = 'rf', metric = "ROC", tuneGrid=tunegrid, trControl = trC)
+fit <- train(Camptothecin~., data = training_set, method = 'rf', metric = "ROC", tuneGrid=tunegrid, trControl = trC)
 fit
 
 
@@ -59,14 +68,14 @@ pred <- predict(fit, newdata = test_set, type = 'prob')
 pred
 
 ## confusionmatrix ##
-confusionMatrix(pred,test_set$CASP10)
+confusionMatrix(pred,test_set$Camptothecin)
 
 
 ## output ##
 
 ## sensitivity, specificity, Matthew’s correlation coefficient ##
 
-mcc = mcc(preds = pred, actuals = training_set$CASP10)
+mcc = mcc(preds = pred, actuals = training_set$Camptothecin)
 
 ## files ##
 
