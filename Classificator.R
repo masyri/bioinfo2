9370a55ad84903b85ed20b599ea656e11c8bd687
Mareike Syring <masy00001@stud.uni-saarland.de>
2022-06-23 17:51:09 +0200

    update

--- Classificator.R
+++ Classificator.R
@@ -1,22 +1,22 @@
 #
-#  ``` $ ./Rscript Classificator.R <gene expression> <drug> <training set 80%> <test set 20%>``` 
+#  ``` $ Rscript Classificator.R <gene expression> <drug> <training set 80%> <test set 20%>``` 
 #
 library(randomForest)
 library(caret)
 library(tidyselect)
-library(mccr)
 library(mltools)
+library(ROSE)
 source("data.R")
 
 
-## load and check arguments
 
+## load and check arguments
+args = commandArgs(trailingOnly=TRUE)
 timestamp <- startTimer()
 input <- loadArgs(args)
 
 ## open files from arguments
-
-drug <- input@drug
+cat("\n => load argument files ... \n")
 drug_data <- openTable(input@drug_data)            
 genes <- openTable(input@gene_expressions)
 training_set <- read.table(input@training_set, header = FALSE)
@@ -25,30 +25,20 @@ test_set <- read.table(input@test_set, header = FALSE)
 drugs <- drug_data[row.names(genes),, drop = FALSE]
 
 ## get current Drug Name
+cat("\n => prepare data ... \n")
 drug_name = names(drugs)[1]
 
 ## feature selection
-## selektieren von bestimmten Krebsgenen 
-
-#cancer.genes <- openTable("data/cancer_gene_list.txt")
-
-#cancer.genes <- as.character(cancer.genes[,1])
-
-#genes <- genes[, colnames(genes) %in% cancer.genes]
-
-
-# fragen ob wir als input nur 1 drug bekommen oder matrix an drugs
-drugs <- subset(drugs, select = c(Camptothecin))
-#genes <- subset(genes, select = c(TSPAN6, TNMD, DPM1))
-
+## selektieren von bestimmten Krebsgenen
+cancer.genes <- openTable("data/cancer_gene_list.txt")
+cancer.genes <- as.character(cancer.genes[,1])
+genes <- genes[, colnames(genes) %in% cancer.genes]
 
 ## combind data
 gesamt <- cbind(drugs, genes)
 
-
 #erase NA values
 gesamt = gesamt[complete.cases(gesamt), ]
-
 
 ## Bind and Split
 training <- as.character(training_set[,1])
@@ -60,14 +50,13 @@ test_matrix = gesamt[test,]
 training_matrix = training_matrix[complete.cases(training_matrix), ]
 test_matrix = test_matrix[complete.cases(test_matrix), ]
 
-
 ## 5-Fold Crossvalidation ##
-trC <- trainControl(method = 'cv', number = 5, classProbs = TRUE, summaryFunction = twoClassSummary)
+trC <- trainControl(method = 'cv', number = 5, classProbs = TRUE, summaryFunction = twoClassSummary, sampling = "rose", verboseIter = T)
 
 ## mtry ###
-tunegrid <- expand.grid(.mtry=c(1:3))
+tunegrid <- expand.grid(.mtry=c(2:10))
 
-# values from 0, 1 to factor
+# values from 0, 1 to factor X0 X1
 training_matrix[,drug_name] = factor(training_matrix[,drug_name])
 levels(training_matrix[,drug_name]) <- make.names(levels(factor(training_matrix[,drug_name])))
 
@@ -78,11 +67,11 @@ levels(test_matrix[,drug_name]) <- make.names(levels(factor(test_matrix[,drug_na
 train_response = training_matrix[, drug_name]
 train_exe = training_matrix[, !(colnames(training_matrix)%in% drug_name)]
 
-
 ## Train ##
+cat("\n => training ... \n")
 fit <- train(x = train_exe, y= train_response, method = 'rf', metric = "ROC", tuneGrid=tunegrid, trControl = trC)
 fit
-
+cat("\n => training finished \n")
 
 
 ## confusionmatrix ##
@@ -91,22 +80,17 @@ pred_test2
 con_m <- confusionMatrix(pred_test2, test_matrix[,drug_name])
 con_m
 
-## Plot
-
-varImpPlot(x = fit$finalModel , sort = TRUE)
-
-
-## output ##
-
+## results ##
 ## sensitivity, specificity, Matthew’s correlation coefficient ##
+
 #  cross validation results
 finalm <- fit$bestTune$mtry
 cv_results <- fit$results
 cv_sens <- cv_results[cv_results$mtry == finalm , "Sens"]
 cv_spec <- cv_results[cv_results$mtry == finalm , "Spec"]
-cv_mcc <- mccr(act = test_matrix[,drug_name],pred = pred_test2)
+cv_mcc <- mcc(actuals = test_matrix[,drug_name],preds = pred_test2)
 
-#  specificity
+#  test results
 test_sens <- as.numeric(con_m$byClass["Sensitivity"])
 test_spec <- as.numeric(con_m$byClass["Specificity"])
 
@@ -120,30 +104,65 @@ test_mcc <- mcc(TP = tp, FP = fp, TN = tn, FN = fn)
 
 ## -- OUTPUT -- ##
 
-dir.create("output")
-folderpath <- paste("output/",drug,"/", sep = "")
-dir.create(folderpath)
+dir.create("output",showWarnings = FALSE)
+folderpath <- paste("output/",drug_name,"/", sep = "")
+dir.create(folderpath,showWarnings = FALSE)
 
-dev.copy(jpeg,filename=paste(folderpath,"plot.jpg"));
+
+## Plots ##
+
+## Plot 1
+varImpPlot(x = fit$finalModel , sort = TRUE, main = drug_name)
+dev.copy(jpeg,filename=paste(folderpath,"plot1.jpg"));
 dev.off ();
+cat("\nplot 1\n")
 
+## Plot 2
+plot(fit, main = drug_name)
+dev.copy(jpeg,filename=paste(folderpath,"plot2.jpg"));
+dev.off ();
+cat("\nplot 2\n")
+
+## Plot 3
+plot(fit$finalModel, main = "Barplot")
+dev.copy(jpeg,filename=paste(folderpath,"plot3.jpg"));
+dev.off ();
+cat("\nplot 3\n")
+
+## Plot 4
+plot(pred_test2, test_matrix[,drug_name], xlab = "predicted", ylab = "actuals")
+dev.copy(jpeg,filename=paste(folderpath,"plot4.jpg"));
+dev.off ();
+cat("\nplot 4\n")
+
+## Plot 5
+b_plot <- c(cv_sens,cv_spec,cv_mcc)
+barplot(b_plot,horiz = FALSE)
+dev.copy(jpeg,filename=paste(folderpath,"plot5.jpg"));
+dev.off ();
+cat("\nplot 5\n")
+
+## Error File
 tab <- data.frame(sensitivity= c(cv_sens, test_sens), specificity= c(cv_spec, test_spec), mcc = c(cv_mcc,test_mcc))
 rownames(tab) <- c("CV Error","Test Error")
 write.table(tab, file = paste(folderpath,"error_file.txt"), dec = ',', sep = '\t')
 
+## Training result file
 pred_train3 <- predict(fit, newdata = training_matrix, type = 'raw')
 train_result <- data.frame(Predicted_Response = pred_train3)
 rownames(train_result) <- rownames(training_matrix)
 write.table(train_result, file = paste(folderpath,"training_set_results.txt"), dec = ',', sep = '\t')
 
+## Test result file
 pred_test3 <- predict(fit, newdata = test_matrix, type = 'raw')
 test_result <- data.frame(Predicted_Response = pred_test3)
 rownames(test_result) <- rownames(test_matrix)
 write.table(test_result, file = paste(folderpath,"test_set_results.txt"), dec = ',', sep = '\t') 
 
+## out
 cat("\n\n => Output-Files saved in ",folderpath,"\n")
-
 
 ## -- PROGRAM END -- ##
 
 cat("\n\n => Program finished in ",endTimer(timestamp),"seconds\n")
+
